7158acb8f345b228497f8ff1336e2be7c3048d4b
natejcq <113688841+natejcq@users.noreply.github.com>
2023-02-19 00:48:27 +0530

    Exercise 1 CALCULATOR

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+#Exercise 1 
+#Creating a simple calculator using arithmetic functions
+a = int(input("Enter the first Number i.e. a"))
+b = int(input("Enter the second Number i.e. b"))
+print(f"Addition is {a+b} ")
+print(f"Subtraction is {a-b}")
+print(f"Multiplication is {a*b}")
+print(f"Division is {a/b}")
+print(f"a raised to b is {a**b}")
